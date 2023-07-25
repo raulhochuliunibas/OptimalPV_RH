6ae9ec41b76b58882772841432fb6be9b0c43da5
raulhochuliunibas <raul.hochuli@unibas.ch>
2023-07-25 15:55:33 +0200

    gdf works

--- main_file.py
+++ main_file.py
@@ -6,7 +6,6 @@ import xarray as xr
 import datashader as ds 
 import contextily as ctx
 import matplotlib as mpl
-import fiona
 import pyogrio
 
 from shapely import geometry
@@ -15,20 +14,25 @@ wd_path = "C:/Models/OptimalPV"
 data_path = "C:/Models/data"
 os.chdir(wd_path)   
 os.listdir()
-os.listdir(f'{data_path}')
+os.listdir(f'{data_path}/solarenergie-eignung-daecher_2056.gdb/SOLKAT_DACH_20230221.gdb')
 
-ch_kt = 0
-gpd.read_file(f'{data_path}/data/swissboundaries3d_2023-01_2056_5728.shp')
+ch_kt = gpd.read_file(f'{data_path}/swissboundaries3d_2023-01_2056_5728.shp', layer ='swissBOUNDARIES3D_1_4_TLM_KANTONSGEBIET')
+wgs84_crs = ch_kt.crs.to_string().split(" +up")[0]
+ch_kt = ch_kt.to_crs(wgs84_crs)
+
+ch_gm = gpd.read_file(f'{data_path}/swissboundaries3d_2023-01_2056_5728.shp', layer ='swissBOUNDARIES3D_1_4_TLM_HOHEITSGEBIET')
+wgs84_crs = ch_gm.crs.to_string().split(" +up")[0]
+ch_gm = ch_gm.to_crs(wgs84_crs)
+
+#dat_solkat = gpd.read_file(f'{data_path}/solarenergie-eignung-daecher_2056.gdb/SOLKAT_DACH_20230221.gdb', layer ='SOLKAT_CH_DACH')
+"""
+ch_kt = ch_kt.to_crs(new_crs)
 
 
 
+dat_solkat <- st_read(dsn = paste0(wd_path,"/solarenergie-eignung-daecher_2056.gdb/SOLKAT_DACH_20230221.gdb"),layer = "SOLKAT_CH_DACH")
 
-
-
-
-
-
-
+"""
 
 
 print("****************************** \nfinished running main_file.py \n******************************")
