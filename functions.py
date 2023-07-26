aa9e46516e8e522d7270dd7be2d077ac9640d257
Raul Hochuli <raul.hochuli@unibas.ch>
2023-07-26 09:37:55 +0200

    adding first functions

---
+++ functions.py
@@ -0,0 +1,12 @@
+import os as os
+
+
+def crs2wsg84(gdf_fnc):
+    """
+    Function to convert a geodataframe to wsg84
+    """
+    wgs84_crs = gdf_fnc.crs.to_string().split(" +up")[0]
+    gdf_fnc = gdf_fnc.to_crs(wgs84_crs)
+    return gdf_fnc
+
+
