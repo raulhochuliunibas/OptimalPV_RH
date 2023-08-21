15eb30449b2e9fe4680535e15f93cadc7aeb5b4f
raulhochuliunibas <raul.hochuli@unibas.ch>
2023-08-21 18:36:59 +0200

    first loop made to create hhould specific shapes

--- main_file.py
+++ main_file.py
@@ -4,18 +4,20 @@ import os as os
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import pyogrio
+
 from functions import crs2wsg84
 from datetime import datetime
+from shapely.ops import unary_union
 #from pandasgui import show
 
 
-# pre setup -------------------------------------------------------------
+
+# pre setup + working directory -------------------------------------------------------------
 check = f'\n\n******************************\n started running main_file.py \n start at:{datetime.now()} \n******************************\n\n'
 print(check)
 with open(f'log_file.txt', 'w') as log_file:
     log_file.write(f'{check}\n')
 
-# set working directory -----------------------------------------------
 wd_path = "C:/Models/OptimalPV_RH"
 data_path = "C:/Models/OptimalPV_RH_data"
 os.chdir(wd_path)   
@@ -28,80 +30,96 @@ os.listdir(f'{data_path}/ch.bfe.elektrizitaetsproduktionsanlagen')
 
 # load administrative shapes
 kt_shp = gpd.read_file(f'{data_path}/swissboundaries3d_2023-01_2056_5728.shp', layer ='swissBOUNDARIES3D_1_4_TLM_KANTONSGEBIET')
-wgs84_crs = kt_shp.crs.to_string().split(" +up")[0]
-kt_shp = kt_shp.to_crs(wgs84_crs)
-
 gm_shp = gpd.read_file(f'{data_path}/swissboundaries3d_2023-01_2056_5728.shp', layer ='swissBOUNDARIES3D_1_4_TLM_HOHEITSGEBIET')
-wgs84_crs = gm_shp.crs.to_string().split(" +up")[0]
-gm_shp = gm_shp.to_crs(wgs84_crs)
-
 check = f'* finished loading administrative shapes: {datetime.now()}'
 print(check)
 with open(f'log_file.txt', 'a') as log_file:
     log_file.write(f"{check}\n")
 
-
-# ----------------------------------------------------------------------
-# book mark ------------------------------------------------------------
-# ----------------------------------------------------------------------
-
-
 # load solar kataster shapes
 roof_kat = gpd.read_file(f'{data_path}/solarenergie-eignung-daecher_2056.gdb/SOLKAT_DACH_20230221.gdb', layer ='SOLKAT_CH_DACH')
-roof_kat.head()
-kt_shp.head()
-gm_shp.head()   
-kt_shp.columns
-
-
-kt_shp_be = kt_shp.loc[kt_shp["KANTONSNUM"] == 13].copy(deep=True)
-kt_shp_be.plot(figsize=(10,10))
-plt.show()
-kt_shp_be.head()
-
-roof_kat_be = gpd.sjoin(roof_kat, kt_shp_be, op='within')
-
-"""
-dat_solkat = gpd.read_file(f'{data_path}/solarenergie-eignung-daecher_2056.gdb/SOLKAT_DACH_20230221.gdb', layer ='SOLKAT_CH_DACH')
-wgs84_crs = dat_solkat.crs.to_string().split(" +up")[0]
-dat_solkat = dat_solkat.to_crs(wgs84_crs)
-"""
-
-check = f'* loaded solar kataster shapes: {datetime.now()}'
+check = f'* finished loading roof kataster shapes: {datetime.now()}'
 print(check)
 with open(f'log_file.txt', 'a') as log_file:
     log_file.write(f"{check}\n")
 
-# load pv installations
-"""
-dat_pv = gpd.read_file(f'{data_path}/ch.bfe.elektrizitaetsproduktionsanlagen', layer ='subcat_2_pv')
-wgs84_crs = dat_pv.crs.to_string().split(" +up")[0]
-dat_pv = dat_pv.to_crs(wgs84_crs)
-"""
 
-check = f'* loaded solar kataster shapes: {datetime.now()}'
+
+# set crs to EPSG 4326 ----------------------------------------------------------
+
+kt_shp.set_crs("EPSG:4326", allow_override=True, inplace=True)
+gm_shp.set_crs("EPSG:4326", allow_override=True, inplace=True)
+roof_kat.set_crs("EPSG:4326", allow_override=True, inplace=True)
+
+check = f'\n* changed all CRS to EPSG 4326: {datetime.now()}'
 print(check)
 with open(f'log_file.txt', 'a') as log_file:
     log_file.write(f"{check}\n")
-
-# create roofs from kataster shapes ------------------------------------
-shp_roof = gm_shp.unary_union
-
 
 
 # ----------------------------------------------------------------------
 # book mark ------------------------------------------------------------
 # ----------------------------------------------------------------------
+print("\n\n subset roof_kat for certain building types \n******************************\n\n")
+"""
+0 Bruecke gedeckt
+1 Gebaeude Einzelhaus
+2 Hochhaus
+3 Hochkamin
+4 Turm
+5 Kuehlturm
+6 Lagertank
+7 Lueftungsschacht
+8 Offenes Gebaeude
+9 Treibhaus
+10 Im Bau
+11 Kapelle
+12 Sakraler Turm
+13 Sakrales Gebaeude
+15 Flugdach
+16 Unterirdisches Gebaeude
+17 Mauer gross
+18 Mauer gross gedeckt
+19 Historische Baute
+20 Gebaeude unsichtbar
+"""
+cat_sb_object = [2,4]#[1,2,4,5,8,12,13]
+roof_kat['SB_OBJEKTART'].value_counts()
+roof_kat2 = roof_kat.loc[roof_kat['SB_OBJEKTART'].isin(cat_sb_object)].copy()
 
 
-kt_shp_zh = kt_shp[kt_shp['NAME'] == 'Zürich']
+roof_kat2.to_file(f'{data_path}/roof_kat2.shp')
+roof_kat.columns
+sb_obj_unique = roof_kat2['SB_UUID'].unique() 
+type(sb_obj_unique)
+
+pd_union = pd.DataFrame(index = sb_obj_unique, columns = ['polygon_geom'])
+
+ctoff_roof_kat_area = [10,300] #TODO: add here values from the PV installation data set
+for idx, n_row in pd_union.iterrows():
+    # add unified geometry
+    pd_union.loc[idx, 'polygon_geom'] = roof_kat2.loc[roof_kat2['SB_UUID'] == idx, 'geometry'].unary_union
+    # add roof area cat_1to3
+    
+
+roof_union = gpd.GeoDataFrame(pd_union, geometry = 'polygon_geom', crs = roof_kat2.crs)
+roof_union.to_file(f'{data_path}/roof_union.shp')
+
+check = f'\n* created roof_union with unified geometriey + exported to shp: {datetime.now()}'
+print(check)
+with open(f'log_file.txt', 'a') as log_file:
+    log_file.write(f"{check}\n")
 
 
-asdf = gpd.sjoin(gm_shp, kt_shp_zh, how='inner', op='intersects')
-type(asdf)
 
 
-
-
-print("stuff acutally happend")
 print("\n\nfinished running main_file.py \n******************************\n\n")
+
+# ----------------------------------------------------------------------
+# book mark ------------------------------------------------------------
+# ----------------------------------------------------------------------
+roof_kat2.columns
+asdf = sb_obj_unique[7]
+roof_kat2.loc[roof_kat2['SB_UUID'] == asdf & roof_kat2[], 'geometry']
+# add roof area cat_1to3
+pd_union.loc[idx, 'roof_area'] = roof_kat2.loc[roof_kat2['SB_UUID'] == idx, 'FLAECHE'].sum()
