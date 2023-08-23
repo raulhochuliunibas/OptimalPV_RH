515d2d07a9b658cf6d85dd9c0c6f3e1ebd0649cb
raulhochuliunibas <raul.hochuli@unibas.ch>
2023-08-23 15:40:02 +0200

    .

--- functions.py
+++ functions.py
@@ -1,18 +1,15 @@
 import os as os
 from datetime import datetime
 
-
-def crs2wsg84(gdf_fnc):
+def chapter_to_logfile(str):
     """
-    Function to convert a geodataframe to wsg84
+    Function to write a chapter to the logfile
     """
-    wgs84_crs = gdf_fnc.crs.to_string().split(" +up")[0]
-    gdf_fnc = gdf_fnc.to_crs(wgs84_crs)
-    return gdf_fnc
+    check = f'\n\n****************************************\n {str} \n start at:{datetime.now()} \n****************************************\n\n'
+    print(check)
+    with open(f'log_file.txt', 'a') as log_file:
+        log_file.write(f'{check}\n')
 
-# ----------------------------------------------------------------------
-# book mark ------------------------------------------------------------
-# ----------------------------------------------------------------------
 
 
 def checkpoint_to_logfile(str):
@@ -24,12 +21,7 @@ def checkpoint_to_logfile(str):
     with open(f'log_file.txt', 'a') as log_file:
         log_file.write(f"{check}\n")
 
-def chapter_to_logfile(str):
-    """
-    Function to write a chapter to the logfile
-    """
-    check = f'\n\n****************************************\n {str} \n start at:{datetime.now()} \n****************************************\n\n'
-    print(check)
-    with open(f'log_file.txt', 'a') as log_file:
-        log_file.write(f'{check}\n')
 
+# ----------------------------------------------------------------------
+# book mark ------------------------------------------------------------
+# ----------------------------------------------------------------------
