f85c0bdba74bc5c9c6099ce7d88b3b0822894595
raulhochuliunibas <raul.hochuli@unibas.ch>
2023-08-24 13:21:00 +0200

    pv added but not yet functional to loop cannot display gdf

--- main_file.py
+++ main_file.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import pyogrio
 import winsound
 
-from functions import checkpoint_to_logfile
+from functions import chapter_to_logfile, checkpoint_to_logfile
 from datetime import datetime
 from shapely.ops import unary_union
 
@@ -32,41 +32,43 @@ chapter_to_logfile('started running main_file.py')
 os.listdir(f'{data_path}/ch.bfe.elektrizitaetsproduktionsanlagen')
 
 
-
 # import data ----------------------------------------------------------
 
 # load administrative shapes
 kt_shp = gpd.read_file(f'{data_path}/swissboundaries3d_2023-01_2056_5728.shp', layer ='swissBOUNDARIES3D_1_4_TLM_KANTONSGEBIET')
+kt_shp.set_crs("EPSG:4326", allow_override=True, inplace=True)
 gm_shp = gpd.read_file(f'{data_path}/swissboundaries3d_2023-01_2056_5728.shp', layer ='swissBOUNDARIES3D_1_4_TLM_HOHEITSGEBIET')
+gm_shp.set_crs("EPSG:4326", allow_override=True, inplace=True)
+
 checkpoint_to_logfile(f'finished loading administrative shapes')
 
 # load solar kataster shapes
 roof_kat = gpd.read_file(f'{data_path}/solarenergie-eignung-daecher_2056.gdb/SOLKAT_DACH_20230221.gdb', layer ='SOLKAT_CH_DACH')
+roof_kat.set_crs("EPSG:4326", allow_override=True, inplace=True)
 checkpoint_to_logfile(f'finished loading solar kataster shapes')
 
 # load pv installation points
 pv = gpd.read_file(f'{data_path}/ch.bfe.elektrizitaetsproduktionsanlagen', layer = 'subcat_2_pv')
-pv.head()
+pv.set_crs("EPSG:4326", allow_override=True, inplace=True)  
+
+# check if all CRS are compatible
+kt_shp.crs == gm_shp.crs == roof_kat.crs == pv.crs
+
 checkpoint_to_logfile(f'finished loading pv installation points')
-
-
-# set crs to EPSG 4326 ----------------------------------------------------------
-
-kt_shp.set_crs("EPSG:4326", allow_override=True, inplace=True)
-gm_shp.set_crs("EPSG:4326", allow_override=True, inplace=True)
-roof_kat.set_crs("EPSG:4326", allow_override=True, inplace=True)
-checkpoint_to_logfile(f'finished setting crs to EPSG 4326')
-
 
 
 
 # ----------------------------------------------------------------------
 # Aggregate roof parts at house level 
 # ----------------------------------------------------------------------
-chapter_to_logfile('start aggr. roof parts at house level')
+chapter_to_logfile('aggregate roof parts at house level')
+winsound.Beep(840,  100)
+winsound.Beep(840,  100)
 
 
-# subset to relevant houses ----------------------------------------------------------
+# set aggregate parameters  ----------------------------------------------------------
+
+# subset to relevant houses 
 """
 0 Bruecke gedeckt
 1 Gebaeude Einzelhaus
@@ -92,68 +94,114 @@ chapter_to_logfile('start aggr. roof parts at house level')
 cat_sb_object = [2,4]#[1,2,4,5,8,12,13]
 roof_kat['SB_OBJEKTART'].value_counts()
 roof_kat_sub = roof_kat.loc[roof_kat['SB_OBJEKTART'].isin(cat_sb_object)].copy()
-# export subset of relevant roof kat 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    roof_kat_sub.to_file(f'{data_path}/roof_kat2.shp', driver='ESRI Shapefile')
+
+# define roof range which are considered
+cutoff_roof_kat_area = [10,300] #TODO: add here values from the PV installation data set
 
 
-# create empty nan df pd_union with unique sb_obj_uuids as index ----------------------------------------------------------
+# roof kataster: create empty nan gdf with unique sb_obj_uuids as index ----------------------------------------------------------
 sb_obj_unique = roof_kat_sub['SB_UUID'].unique() 
-pd_union = pd.DataFrame(index = sb_obj_unique, columns = ['polygon_geom'])
+roof_union = gpd.GeoDataFrame(index = sb_obj_unique, columns = ['geometry']) #TODO: find a better naming convetion!
 cols = ['FLAECHE', 'MSTRAHLUNG', 'GSTRAHLUNG', 'STROMERTRAG']
 cats = ['cat2_', 'cat3_', 'cat4_', 'cat5_']
 new_col = [cat + col for cat in cats for col in cols ]
-pd_union[new_col] = np.nan
+roof_union[new_col] = np.nan
 checkpoint_to_logfile(f'created empty df for iter over roof parts')
 
 
-# loop ----------------------------------------------------------------------
-# iterating over all SB_UUID, adding roof_kat data by individual house 
-cutoff_roof_kat_area = [10,300] #TODO: add here values from the PV installation data set
-for idx, row_srs in pd_union.iterrows():
+# roof kataster: loop over roof_kat  ----------------------------------------------------------------------
+
+for idx, row_srs in roof_union.iterrows():
     
     # add unified geometry
-    row_srs['polygon_geom'] = roof_kat_sub.loc[roof_kat_sub['SB_UUID'] == idx, 'geometry'].unary_union
-    
+    #row_srs['geometry'] = roof_kat_sub.loc[roof_kat_sub['SB_UUID'] == idx, 'geometry'].unary_union
+    roof_union.loc[idx, 'geometry'] = roof_kat_sub.loc[roof_kat_sub['SB_UUID'] == idx, 'geometry'].unary_union
+    """
     # set boolean indicatros 
     bool_id_2 = (roof_kat_sub['KLASSE'].isin([1,2])) & (roof_kat_sub['SB_UUID'] == idx ) & (roof_kat_sub['FLAECHE'] > cutoff_roof_kat_area[0]) & (roof_kat_sub['FLAECHE'] < cutoff_roof_kat_area[1])
     bool_id_3 = (roof_kat_sub['KLASSE'].isin([3]))   & (roof_kat_sub['SB_UUID'] == idx ) & (roof_kat_sub['FLAECHE'] > cutoff_roof_kat_area[0]) & (roof_kat_sub['FLAECHE'] < cutoff_roof_kat_area[1])
     bool_id_4 = (roof_kat_sub['KLASSE'].isin([4]))   & (roof_kat_sub['SB_UUID'] == idx ) & (roof_kat_sub['FLAECHE'] > cutoff_roof_kat_area[0]) & (roof_kat_sub['FLAECHE'] < cutoff_roof_kat_area[1])
     bool_id_5 = (roof_kat_sub['KLASSE'].isin([5]))   & (roof_kat_sub['SB_UUID'] == idx ) & (roof_kat_sub['FLAECHE'] > cutoff_roof_kat_area[0]) & (roof_kat_sub['FLAECHE'] < cutoff_roof_kat_area[1])
 
+    # add roof part values to aggr df
     for col in cols: 
-        pd_union.loc[idx, f'cat2_{col}'] = roof_kat_sub.loc[bool_id_2, f'{col}'].sum()
-        pd_union.loc[idx, f'cat3_{col}'] = roof_kat_sub.loc[bool_id_3, f'{col}'].sum()
-        pd_union.loc[idx, f'cat4_{col}'] = roof_kat_sub.loc[bool_id_4, f'{col}'].sum()
-        pd_union.loc[idx, f'cat5_{col}'] = roof_kat_sub.loc[bool_id_5, f'{col}'].sum()
+        roof_union.loc[idx, f'cat2_{col}'] = roof_kat_sub.loc[bool_id_2, f'{col}'].sum()
+        roof_union.loc[idx, f'cat3_{col}'] = roof_kat_sub.loc[bool_id_3, f'{col}'].sum()
+        roof_union.loc[idx, f'cat4_{col}'] = roof_kat_sub.loc[bool_id_4, f'{col}'].sum()
+        roof_union.loc[idx, f'cat5_{col}'] = roof_kat_sub.loc[bool_id_5, f'{col}'].sum()
+    """
 
 checkpoint_to_logfile(f'finished loop iter over roof parts')
-winsound.Beep(840,  100)
-winsound.Beep(840,  100)
 
 
+# pv inst: add empty nan conlumns to aggr df   ----------------------------------------------------------------------
 
-# transform pd to gdf ----------------------------------------------------------
-roof_union = gpd.GeoDataFrame(pd_union, geometry = 'polygon_geom', crs = roof_kat2.crs)
-roof_union.to_file(f'{data_path}/roof_union.shp')
-checkpoint_to_logfile(f'pd.df transformed to geo df + exported')
+roof_union['pv_d', 'pv_address', 'pv_postcode', 'pv_BeginningO', 'InitialPow', 'TotalPow' ] = np.nan
 
 
+# pv inst: loop over pv   ----------------------------------------------------------------------
 
-# END ----------------------------------------------------------------------
-chapter_to_logfile('END of main_file.py')
-winsound.Beep(200, 300)
-winsound.Beep(38,  150)
-winsound.Beep(200, 300)
-winsound.Beep(38,  150)
-winsound.Beep(200, 1000)
+for idx, row_srs in roof_union.iterrows():
+
+    bool_id_pvonroof = pv['geometry'].intersects(roof_union.loc[idx, 'geometry'].buffer(0, resolution = 16))
+    roof_union.loc[idx, 'pv_d'] = bool_id_pvonroof.sum()
+    """
+    roof_union.loc[idx, 'pv_address'] 
+    roof_union.loc[idx, 'pv_postcode']
+    roof_union.loc[idx, 'pv_BeginningO']
+    roof_union.loc[idx, 'InitialPow']
+    roof_union.loc[idx, 'TotalPow']
+    """
 
 # ----------------------------------------------------------------------
 # book mark ------------------------------------------------------------
+roof_union['pv_d'].value_counts()
+
 # ----------------------------------------------------------------------
 
-#> asdfasdf
+
+# export ----------------------------------------------------------
+roof_union.to_file(f'{data_path}/roof_union.shp')
+roof_union.to_file(f'{data_path}/roof_union.geojson', driver='GeoJSON')
+checkpoint_to_logfile(f'aggregated gdf exported')
+
+
+# ----------------------------------------------------------------------
+# END ------------------------------------------------------------------
+# ----------------------------------------------------------------------
+chapter_to_logfile('END of main_file.py')
+winsound.Beep(400, 100)
+winsound.Beep(400, 100)
+winsound.Beep(400, 500)
+
+
+type(roof_union)
+roof_union.crs
+roof_union.head()
+roof_union.info()
+roof_union['geometry'].value_counts()
+roof_union['geometry'].plot()
+plt.show()
 
 
 
+
+roof_union.columns
+type(roof_union)
+type(roof_union['geometry'])
+roof_union ['geometry']
+roof_union.loc[idx, 'geometry'].buffer(1, resolution = 16)
+type(pv['geometry'])
+
+
+
+
+
+kt_shp = gpd.read_file(f'{data_path}/swissboundaries3d_2023-01_2056_5728.shp', layer ='swissBOUNDARIES3D_1_4_TLM_KANTONSGEBIET')
+kt_shp.set_crs("EPSG:4326", allow_override=True, inplace=True)
+
+kt_shp.plot()
+plt.show()
+
+gm_shp = gpd.read_file(f'{data_path}/swissboundaries3d_2023-01_2056_5728.shp', layer ='swissBOUNDARIES3D_1_4_TLM_HOHEITSGEBIET')
+gm_shp.set_crs("EPSG:4326", allow_override=True, inplace=True)
