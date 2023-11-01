42429500bfaa359c9e8bb27d9a39c1913af3ae33
raulhochuliunibas <raul.hochuli@unibas.ch>
2023-11-01 17:40:52 +0100

    update plotting file

--- plots_proposal.py
+++ plots_proposal.py
@@ -9,13 +9,12 @@ import winsound
 import plotly 
 import plotly.express as px
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+import plotly.offline as pyo
 
 from functions import chapter_to_logfile, checkpoint_to_logfile
-from datetime import datetime
-from shapely.ops import unary_union
 
-import warnings
+from plotly.subplots import make_subplots
+from datetime import datetime
 
 
 # pre run settings -----------------------------------------------------------------------------------------------
@@ -37,13 +36,14 @@ chapter_to_logfile('started plots_proposal.py')
 
 # set working directory
 if script_run_on_server == 0:
-     winsound.Beep(840,  100)
-     wd_path = "C:/Models/OptimalPV_RH"   # path for private computer
+    winsound.Beep(840,  100)
+    wd_path = "C:/Models/OptimalPV_RH"   # path for private computer
 elif script_run_on_server == 1:
-     wd_path = "D:\OptimalPV_RH"         # path for server directory
+    wd_path = "D:\OptimalPV_RH"         # path for server directory
 
 data_path = f'{wd_path}_data'
 os.chdir(wd_path)
+
 
 # import data -----------------------------------------------------------------------------------------------------
 
@@ -85,7 +85,7 @@ elec_prod['SubCategory'] = np.select(case, when)
 
 # Grouping by month and 'SubCategor', then aggregating by count and sum of 'TotalPow'
 select_col = 'Counts'  # 'Counts' or 'PowerSum'
-elec_prod_ts_agg = elec_prod.groupby([elec_prod['Date'].dt.to_period("W"), 'SubCategory']).agg(
+elec_prod_ts_agg = elec_prod.groupby([elec_prod['Date'].dt.to_period("Y"), 'SubCategory']).agg(
      Counts=('xtf_id', 'size'), 
      PowerSum=('InitialPower', 'sum')
      ).reset_index()
@@ -93,18 +93,18 @@ elec_prod_ts_agg = elec_prod.groupby([elec_prod['Date'].dt.to_period("W"), 'SubC
 elec_prod_ts_agg['Date'] = elec_prod_ts_agg['Date'].dt.to_timestamp() # Convert 'BeginningO' back to timestamp for plotting
 elec_prod_ts_agg.info()
 
-# plot ELECTRICITS PROD OVER TIME --------------------------
 
-def plot_pv_counts_ts(show_plot = True):
+# plot ELECTRICITS PROD OVER TIME ------------------------------------------------------------------------------------
+def plot_pv_counts_ts(show_plot = True, export_plot = False):
 
     # plotting
     fig = px.line(elec_prod_ts_agg, x='Date', y=f'{select_col}', color='SubCategory', title=f'Monthly {select_col} of SubCategor Entries')
     fig.update_layout(
-    plot_bgcolor='white',
-    paper_bgcolor='white',
-    xaxis=dict(showgrid=True, gridcolor='lightgray'),
-    yaxis=dict(showgrid=True, gridcolor='lightgray')
-    )
+        plot_bgcolor='white',
+        paper_bgcolor='white',
+        xaxis=dict(showgrid=True, gridcolor='lightgray'),
+        yaxis=dict(showgrid=True, gridcolor='lightgray')
+        )
 
     # zoom plot
     start_date = '2000-01-01'
@@ -159,8 +159,39 @@ def plot_pv_counts_ts(show_plot = True):
         fig.show()
 plot_pv_counts_ts(False)
 
-# plot PV Counts + PowerSum OVER TIME -----------------------------------
-def plot_pv_2axis_counts_capacity_ts(show_plot = True):
+# plot PV Counts + PowerSum OVER TIME  -------------------------------------------------------------------------------
+title_font = 1
+legend_title_font = 20 # 40
+leged_font = 15 #35
+tick_title_font = 20 #40
+tick_font = 15# 35
+caption_font = 20 #40
+line_size = 3 # 7
+start_date = '1993-01-01'
+end_date = '2023-12-31'
+
+def add_date_caption(fig_func, date_func, str_func, y0_func=0.05, y1_func=0.925):
+    cap_date = date_func
+    fig_func.add_shape(
+        go.layout.Shape(
+            type="line",
+            x0=cap_date,
+            x1=cap_date,
+            y0=y0_func,
+            y1=y1_func,
+            yref="paper",
+            line=dict(color="black", width=1, dash='dash')
+        )
+    )
+    fig_func.add_annotation(
+        x=cap_date,
+        y=0.95,
+        yref="paper",
+        text=str_func,
+        showarrow=False,
+        font=dict(color="black", size = caption_font)
+    )
+def plot_pv_2axis_counts_capacity_ts(show_plot = True, export_plot = False):
 
     # Filter the dataset for SubCategory 'PV'
     df_pv = elec_prod_ts_agg[elec_prod_ts_agg['SubCategory'] == 'PV']
@@ -168,19 +199,19 @@ def plot_pv_2axis_counts_capacity_ts(show_plot = True):
     # Create a subplot with secondary y-axis
     fig2 = make_subplots(specs=[[{"secondary_y": True}]])
 
-    # Add traces for Counts + PowerSum
+    # Add traces for counts + counPowreSum
     fig2.add_trace(
-        go.Scatter(x=df_pv['Date'], y=df_pv['Counts'], mode='lines', name='Counts'),
+        go.Scatter(x=df_pv['Date'], y=df_pv['Counts'], mode='lines', name='Counts', line=dict(color = 'blue', width=line_size)),
         secondary_y=False,
     )
     fig2.add_trace(
-        go.Scatter(x=df_pv['Date'], y=df_pv['PowerSum'], mode='lines', name='PowerSum'),
+        go.Scatter(x=df_pv['Date'], y=df_pv['PowerSum'], mode='lines', name='PowerSum', line=dict(color = 'red', width=line_size)),
         secondary_y=True,
     )
 
     # Update the layout
     fig2.update_layout(
-        title_text='Monthly Counts and PowerSum of SubCategory PV Entries',
+        title_text='Anuall Counts and Capacity of PV Plants in Switzerland',
         plot_bgcolor='white',
         paper_bgcolor='white',
         xaxis=dict(showgrid=True, gridcolor='lightgray'),
@@ -189,57 +220,100 @@ def plot_pv_2axis_counts_capacity_ts(show_plot = True):
     fig2.update_yaxes(title_text="PowerSum", showgrid=True, gridcolor='lightgray', secondary_y=True)
 
     # zoom plot
-    start_date = '2003-01-01'
-    end_date = '2023-12-31'
+    # start_date = '2002-01-01'
+    # end_date = '2023-12-31'
     fig2.update_xaxes(range=[start_date, end_date])
 
-    # add time steps
-    cap_date1 = "2009-01-01"
-    fig2.add_shape(
-        go.layout.Shape(
-            type="line",
-            x0=cap_date1,
-            x1=cap_date1,
-            y0=0.025,
-            y1=0.975,
-            yref="paper",
-            line=dict(color="black", width=1)
-        )
+    add_date_caption(fig2, "2009-01-01", "KEV, 2009", 0.025, 0.925)
+    add_date_caption(fig2, "2014-01-01", "EIV, 2014", )
+
+    # update axes
+    fig2.update_xaxes(
+        title_font=dict(size=tick_title_font),  # Font size for X-axis title
+        tickfont=dict(size=tick_font)     # Font size for X-axis tick labels
     )
-    fig2.add_annotation(
-        x=cap_date1,
-        y=0.95,
-        yref="paper",
-        text="KEV, 2009",
-        showarrow=False,
-        font=dict(color="black")
+    fig2.update_yaxes(
+        title_font=dict(size=tick_title_font),  # Font size for Y-axis title
+        tickfont=dict(size=tick_font)     # Font size for Y-axis tick labels
+    )
+    fig2.update_layout(
+        title_font=dict(size=title_font),    # Font size for figure title
+        legend_title_font=dict(size=legend_title_font),  # Font size for legend title
+        legend_font=dict(size=leged_font)    # Font size for legend items
     )
 
-    cap_date2 = "2014-01-01"
-    fig2.add_shape(
-        go.layout.Shape(
-            type="line",
-            x0=cap_date2,
-            x1=cap_date2,
-            y0=0.025,
-            y1=0.975,
-            yref="paper",
-            line=dict(color="black", width=1)
-        )
-    )
-    fig2.add_annotation(
-        x=cap_date2,
-        y=0.95,
-        yref="paper",
-        text="EIV, 2014",
-        showarrow=False,
-        font=dict(color="black")
-    )
+    # change trace name   
+    fig2.update_traces(name="sum Capacity (kW)", selector=dict(name="PowerSum"))
+    
+    fig2.update_layout(showlegend = False)
+
     if show_plot:
         fig2.show()
+    if export_plot:
+        pyo.plot(fig2, filename=f'{data_path}/output/plot_pv_2axis_counts_capacity_ts.html', auto_open=False)
+    
+    return fig2
 
-plot_pv_2axis_counts_capacity_ts()
+plot_pv_2axis_counts_capacity_ts(show_plot=True, export_plot=False)
+
+# plot PV Growth OVER TIME -----------------------------------------------------------------------------------------
+def plot_pv_2axis_counts_capacity_growth(show_plot = True, export_plot = False):
+    elec_prod_ts_agg = elec_prod.groupby([elec_prod['Date'].dt.to_period("Y"), 'SubCategory']).agg(
+        Counts=('xtf_id', 'size'), 
+        PowerSum=('InitialPower', 'sum')
+        ).reset_index()
+    elec_prod_ts_agg['Date'] = elec_prod_ts_agg['Date'].dt.to_timestamp() # Convert 'BeginningO' back to timestamp for plotting
+    elec_prod_ts_agg.info()
+
+    df_pv = elec_prod_ts_agg[elec_prod_ts_agg['SubCategory'] == 'PV'].copy()
+    # calcucalte growth rates
+    df_pv['Counts_growth'] = (df_pv['Counts'] - df_pv['Counts'].shift(1)) / df_pv['Counts'].shift(1)
+    df_pv['PowerSum_growth'] = (df_pv['PowerSum'] - df_pv['PowerSum'].shift(1)) / df_pv['PowerSum'].shift(1)
+
+    # fig_func = make_subplots(specs=[[{"secondary_y": True}]])
+    fig_func = go.Figure()
+    df_pv.head(10)
+
+    # add traces
+    fig_func.add_trace(go.Scatter(x=df_pv['Date'], y=df_pv['Counts_growth'], mode='lines', name='Counts Growth', line=dict(color = 'blue', width=line_size)))
+    fig_func.add_trace(go.Scatter(x=df_pv['Date'], y=df_pv['PowerSum_growth'], mode='lines', name='PowerSum Growth', line=dict(color = 'red', width=line_size)))
+
+    # Update the layout
+    fig_func.update_layout(
+        title_text='Anuall Growth of Counts and Capacity of PV Plants in Switzerland',
+        plot_bgcolor='white',
+        paper_bgcolor='white',
+        xaxis=dict(showgrid=True, gridcolor='lightgray'),)
+
+    # zoom plot
+    # start_date = '1997-01-01'
+    # end_date = '2023-12-31'
+    fig_func.update_xaxes(range=[start_date, end_date])
+
+    add_date_caption(fig_func, "2009-01-01", "KEV, 2009")
+    add_date_caption(fig_func, "2014-01-01", "EIV, 2014")
+
+    # update axes
+    fig_func.update_xaxes(
+        title_font=dict(size=tick_title_font),  # Font size for X-axis title
+        tickfont=dict(size=tick_font))
+    fig_func.update_yaxes(
+        title_font=dict(size=tick_title_font),  # Font size for Y-axis title
+        tickfont=dict(size=tick_font))
+    fig_func.update_layout(
+        title_font=dict(size=title_font),    # Font size for figure title
+        legend_title_font=dict(size=legend_title_font),  # Font size for legend title
+        legend_font=dict(size=leged_font))    # Font size for legend items
+
+    fig_func.update_traces(name="growth Capacity (kW)", selector=dict(name="PowerSum"))
+    fig_func.update_layout(showlegend = False)
+
+    if show_plot:
+        fig_func.show()
+    if export_plot:
+        pyo.plot(fig_func, filename=f'{data_path}/output/plot_pv_2axis_counts_capacity_growth.html', auto_open=False)
+
+plot_pv_2axis_counts_capacity_growth(show_plot=True, export_plot=False)
 
 
 
-elec_prod['Date']
