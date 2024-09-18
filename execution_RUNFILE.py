c2f238a323f24cacbff67eed959ba83d63b7de3e
raulhochuliunibas <raul.hochuli@unibas.ch>
2024-09-18 13:54:19 +0200

    exec file for server run

--- execution_RUNFILE.py
+++ execution_RUNFILE.py
@@ -32,12 +32,12 @@ dataagg_scenarios = extend_dataag_scen_with_defaults(dataagg_scenarios)
 
 
 # pv_allocation 
-months_pred = 2
+months_pred = 3
 run_on_server = False
 pvalloc_scenarios={
     # BL small sample, 1 y ~ca. 3h 1 scenario
-    f'pvalloc_smallBL_{months_pred}m_npv_weighted': {
-        # 'pvalloc_BLBS_72m_npv_weighted':{
+    # f'pvalloc_smallBL_{months_pred}m_npv_weighted': {
+        'pvalloc_BLBS_72m_npv_weighted':{
             'name_dir_import': 'preprep_BSBLSO_21to22_1and2homes',
             'script_run_on_server': run_on_server,
             'months_prediction': months_pred,
@@ -46,17 +46,30 @@ pvalloc_scenarios={
             'algorithm_specs': {
                 'inst_selection_method': 'prob_weighted_npv',
     }},
-    # f'pvalloc_smallBL_{months_pred}m_random': {
-    # # 'pvalloc_BLBS_72m_random': {
-    #         'name_dir_import': 'preprep_BSBLSO_21to22_1and2homes',
-    #         'script_run_on_server': run_on_server,
-    #         'months_prediction': months_pred,
-    #         'recreate_topology':        True,
-    #         'recalc_economics_topo_df': True,
-    #         'algorithm_specs': {
-    #             'inst_selection_method': 'random',
-    # }}, 
-    # f'pvalloc_smallBL_{months_pred}m_npv_weighted_w_farms': {
+    # # f'pvalloc_smallBL_{months_pred}m_random': {
+    'pvalloc_BLBS_72m_random': {
+            'name_dir_import': 'preprep_BSBLSO_21to22_1and2homes',
+            'script_run_on_server': run_on_server,
+            'months_prediction': months_pred,
+            'recreate_topology':        True,
+            'recalc_economics_topo_df': True,
+            'algorithm_specs': {
+                'inst_selection_method': 'random',
+    }}, 
+
+    'pvalloc_BLBS_72m_npv_weighted_W_export_gdf_partition':{
+            'name_dir_import': 'preprep_BSBLSO_21to22_1and2homes',
+            'script_run_on_server': run_on_server,
+            'months_prediction': months_pred,
+            'recreate_topology':        True,
+            'recalc_economics_topo_df': True,
+            'create_gdf_export_of_topology':    True,
+
+            'algorithm_specs': {
+                'inst_selection_method': 'prob_weighted_npv',
+    }},
+    # # f'pvalloc_smallBL_{months_pred}m_npv_weighted_w_farms': {
+    # 'pvalloc_BLBS_72m_npv_weighted_w_farms': {
     #         'name_dir_import': 'preprep_BSBLSO_18to22_1and2homes_w_farms', 
     #         'script_run_on_server': run_on_server,
     #         'months_prediction': months_pred,
