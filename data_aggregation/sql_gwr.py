cb20f6a40f05bbeffc2c738b23cc1d222964ac90
raulhochuliunibas <raul.hochuli@unibas.ch>
2024-11-20 11:08:32 +0100

    adjustments for more readable summary log file

--- data_aggregation/sql_gwr.py
+++ data_aggregation/sql_gwr.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 import geopandas as gpd
 import  sqlite3
-import re
+import copy
 
 from shapely.geometry import Point
 
@@ -32,6 +32,7 @@ def sql_gwr_data(
     log_file_name_def = dataagg_settings_def['log_file_name']
     wd_path_def = dataagg_settings_def['wd_path']
     data_path_def = dataagg_settings_def['data_path']
+    summary_file_name = dataagg_settings_def['summary_file_name']
 
     gwr_selection_specs_def = dataagg_settings_def['gwr_selection_specs']
     print_to_logfile('run function: sql_gwr_data.py', log_file_name_def=log_file_name_def)
@@ -135,25 +136,18 @@ def sql_gwr_data(
     gwr_all_building_df.to_csv(f'{data_path_def}/output/preprep_data/gwr_building_df.csv', sep=';', index=False)
 
 
-    # merger & filter for specs -------------------
+    # merger -------------------
     # gwr = gwr_building_df.merge(gwr_dwelling_df, on='EGID', how='left')
     gwr_mrg = gwr_all_building_df.merge(gwr_dwelling_df, on='EGID', how='left')
 
 
-    checkpoint_to_logfile(f'check gwr_mrg BEFORE filtering: {gwr_mrg["EGID"].nunique()} unique EGIDs in gwr_mrg.shape {gwr_mrg.shape}, {round(gwr_mrg["EGID"].nunique()/gwr_mrg.shape[0],2)*100} %', log_file_name_def, 3, True)
-    gwr_mrg['GBAUJ'] = gwr_mrg['GBAUJ'].replace('', 0).astype(int)
-    gwr_mrg = gwr_mrg[(gwr_mrg['GSTAT'].isin(gwr_selection_specs_def['GSTAT'])) & 
-              (gwr_mrg['GKLAS'].isin(gwr_selection_specs_def['GKLAS'])) & 
-              (gwr_mrg['GBAUJ'] >= gwr_selection_specs_def['GBAUJ_minmax'][0]) &
-              (gwr_mrg['GBAUJ'] <= gwr_selection_specs_def['GBAUJ_minmax'][1])]
-    gwr_mrg['GBAUJ'] = gwr_mrg['GBAUJ'].replace(0, '').astype(str)
-    checkpoint_to_logfile(f'check gwr_mrg AFTER filtering: {gwr_mrg["EGID"].nunique()} unique EGIDs in gwr_mrg.shape {gwr_mrg.shape}, {round(gwr_mrg["EGID"].nunique()/gwr_mrg.shape[0],2)*100} %', log_file_name_def, 3, True)
-
-    
-
     # aggregate dwelling data per EGID -------------------
-    checkpoint_to_logfile('aggregate dwelling data per EGID', log_file_name_def, 3, True)
-    bldg_agg_cols = gwr_selection_specs_def['building_cols'].copy()
+    print_to_logfile('aggregate dwelling data per EGID', log_file_name_def)
+    print_to_logfile('aggregate dwelling data per EGID', summary_file_name)
+    checkpoint_to_logfile(f'check gwr BEFORE aggregation: {gwr_mrg["EGID"].nunique()} unique EGIDs in gwr_mrg.shape {gwr_mrg.shape}, {round((gwr_mrg["EGID"].nunique())/gwr_mrg.shape[0],2)*100} %', log_file_name_def, 3, True)
+    checkpoint_to_logfile(f'check gwr BEFORE aggregation: {gwr_mrg["EGID"].nunique()} unique EGIDs in gwr_mrg.shape {gwr_mrg.shape}, {round((gwr_mrg["EGID"].nunique())/gwr_mrg.shape[0],2)*100} %', summary_file_name, 3, True)
+
+    bldg_agg_cols = copy.deepcopy(gwr_selection_specs_def['building_cols'])
     bldg_agg_cols.remove('EGID')
     bldg_agg_meth = {col: 'first' for col in bldg_agg_cols}
 
@@ -163,32 +157,68 @@ def sql_gwr_data(
     dwel_agg_meth = {'EWID':concat_strings,'nEWID': 'count', 'WAZIM': 'sum', 'WAREA': 'sum'}
 
     agg_meth = {**bldg_agg_meth, **dwel_agg_meth}
-    gwr = gwr_mrg.groupby('EGID').agg(agg_meth).reset_index()
-    checkpoint_to_logfile(f'check gwr AFTER aggregation: {gwr["EGID"].nunique()} unique EGIDs in gwr.shape {gwr.shape}, {round(gwr["EGID"].nunique()/gwr.shape[0],2)*100} %', log_file_name_def, 3, True)
+    gwr_mrg_after_agg =           gwr_mrg.groupby('EGID').agg(agg_meth).reset_index()
+    gwr_mrg_all_building_in_bfs = gwr_mrg.groupby('EGID').agg(agg_meth).reset_index()
+    gwr_mrg = copy.deepcopy(gwr_mrg_after_agg)
+
+    checkpoint_to_logfile(f'check gwr AFTER aggregation: {gwr_mrg["EGID"].nunique()} unique EGIDs in gwr_mrg.shape {gwr_mrg.shape}, {round((gwr_mrg["EGID"].nunique())/gwr_mrg.shape[0],2)*100} %', log_file_name_def, 3, True)
+    checkpoint_to_logfile(f'check gwr AFTER aggregation: {gwr_mrg["EGID"].nunique()} unique EGIDs in gwr_mrg.shape {gwr_mrg.shape}, {round((gwr_mrg["EGID"].nunique())/gwr_mrg.shape[0],2)*100} %', summary_file_name, 3, True)
 
 
-    # summary log
-    print_to_logfile(f'Building and Dwelling data import:', dataagg_settings_def['summary_file_name'])
-    checkpoint_to_logfile(f'gwr_all_building_df.shape: {gwr_all_building_df.shape}, EGID: {gwr_all_building_df["EGID"].nunique()}', dataagg_settings_def['summary_file_name'], 2, True)
-    checkpoint_to_logfile(f'after selection gwr.shape: {gwr.shape}, EGID: {gwr["EGID"].nunique()} ({round(gwr.shape[0] / gwr_all_building_df.shape[0] * 100, 2)} % of all_bldng_df)', dataagg_settings_def['summary_file_name'], 2, True)
-    checkpoint_to_logfile(f'selection settings: \n\tGSTAT: {gwr_selection_specs_def["GSTAT"]} "only existing bulidings"; \n\tGKLAS: {gwr_selection_specs_def["GKLAS"]} "1110 - building w 1 living space, 1121 - w 2 living spaces, 1276 - agricluture buildings (stables, barns )\n\tGBAUJ_minmax: {gwr_selection_specs_def["GBAUJ_minmax"]} "range of years of construction"', dataagg_settings_def['summary_file_name'], 2, True)
-    checkpoint_to_logfile(f'selection range : \n\tOnly buildings within the BFS municicpalities: {bfs_number_def}', dataagg_settings_def['summary_file_name'], 2, True)
+    # filter for specs -------------------
+    checkpoint_to_logfile(f'check gwr_mrg BEFORE filtering: {gwr_mrg["EGID"].nunique()} unique EGIDs in gwr_mrg.shape {gwr_mrg.shape}, {round((gwr_mrg["EGID"].nunique() )/gwr_mrg.shape[0],2)*100} %', log_file_name_def, 3, True)
+    # gwr_mrg['GBAUJ'] = gwr_mrg['GBAUJ'].replace('', 0).astype(int)
+    # gwr_mrg = gwr_mrg[(gwr_mrg['GSTAT'].isin(gwr_selection_specs_def['GSTAT'])) & 
+    #           (gwr_mrg['GKLAS'].isin(gwr_selection_specs_def['GKLAS'])) & 
+    #           (gwr_mrg['GBAUJ'] >= gwr_selection_specs_def['GBAUJ_minmax'][0]) &
+    #           (gwr_mrg['GBAUJ'] <= gwr_selection_specs_def['GBAUJ_minmax'][1])]
+    # gwr_mrg['GBAUJ'] = gwr_mrg['GBAUJ'].replace(0, '').astype(str)
+    gwr_mrg0 = copy.deepcopy(gwr_mrg)
+    gwr_mrg0['GBAUJ'] = gwr_mrg0['GBAUJ'].replace('', 0).astype(int)
+    gwr_mrg1 = gwr_mrg0[(gwr_mrg0['GSTAT'].isin(gwr_selection_specs_def['GSTAT']))]
+    gwr_mrg2 = gwr_mrg1[(gwr_mrg1['GKLAS'].isin(gwr_selection_specs_def['GKLAS']))]
+    gwr_mrg3 = gwr_mrg2[(gwr_mrg2['GBAUJ'] >= gwr_selection_specs_def['GBAUJ_minmax'][0]) & (gwr_mrg2['GBAUJ'] <= gwr_selection_specs_def['GBAUJ_minmax'][1])]
+    gwr_mrg = gwr_mrg3
+    checkpoint_to_logfile(f'check gwr_mrg AFTER filtering: {gwr_mrg["EGID"].nunique()} unique EGIDs in gwr_mrg.shape {gwr_mrg.shape}, {round((gwr_mrg["EGID"].nunique() )/gwr_mrg.shape[0],2)*100} %', log_file_name_def, 3, True)
+    print_to_logfile(f'\n', log_file_name_def=summary_file_name)
+
+
+    # summary log -------------------
+    print_to_logfile(f'Building and Dwelling data import:', summary_file_name)
+    checkpoint_to_logfile(f'gwr_mrg0.shape: {gwr_mrg0.shape}, EGID: {gwr_mrg0["EGID"].nunique()}', summary_file_name, 2, True)
+    # checkpoint_to_logfile(f'gwr_all_building_df.shape: {gwr_all_building_df.shape}, EGID: {gwr_all_building_df["EGID"].nunique()}', summary_file_name, 2, True)
+    checkpoint_to_logfile(f'\t> selection range BFS municipalities: {bfs_number_def}', summary_file_name, 2, True)
+    # print_to_logfile(f'\n', log_file_name_def=summary_file_name)
+    checkpoint_to_logfile(f'after GSTAT selection, gwr.shape: {gwr_mrg1.shape} EGID.nunique: {gwr_mrg1["EGID"].nunique()} ({round((gwr_mrg1.shape[0] ) / gwr_mrg0.shape[0] * 100, 2)} % of gwr_mrg0)', summary_file_name, 2, True) 
+    checkpoint_to_logfile(f'\t> selection GSTAT: {gwr_selection_specs_def["GSTAT"]} "only existing bulidings"', summary_file_name, 2, True)
+    # print_to_logfile(f'\n', log_file_name_def=summary_file_name)
+    checkpoint_to_logfile(f'after GKLAS selection, gwr.shape: {gwr_mrg2.shape} EGID.nunique: {gwr_mrg2["EGID"].nunique()} ({round((gwr_mrg2.shape[0] ) / gwr_mrg1.shape[0] * 100, 2)} % of gwr_mrg1)', summary_file_name, 2, True)
+    checkpoint_to_logfile(f'\t> selection GKLAS: {gwr_selection_specs_def["GKLAS"]} "1110 - building w 1 living space, 1121 - w 2 living spaces, 1276 - agricluture buildings (stables, barns )"', summary_file_name, 2, True)
+    # print_to_logfile(f'\n', log_file_name_def=summary_file_name)
+    checkpoint_to_logfile(f'after GBAUJ_minmax selection, gwr.shape: {gwr_mrg3.shape} EGID.nunique: {gwr_mrg3["EGID"].nunique()} ({round((gwr_mrg3.shape[0] ) / gwr_mrg2.shape[0] * 100, 2)} % of gwr_mrg3)', summary_file_name, 2, True)
+    checkpoint_to_logfile(f'\t> selection GBAUJ_minmax: {gwr_selection_specs_def["GBAUJ_minmax"]} "built construction between years"', summary_file_name, 2, True)
+    # print_to_logfile(f'\n', log_file_name_def=summary_file_name)
+    checkpoint_to_logfile(f'from ALL gwr_mrg0 (aggregated with dwelling, bfs already selected): {gwr_mrg0["EGID"].nunique()-gwr_mrg3["EGID"].nunique()} of {gwr_mrg0["EGID"].nunique()} EGIDs removed ({round((gwr_mrg0["EGID"].nunique() - gwr_mrg3["EGID"].nunique()  )/gwr_mrg0["EGID"].nunique()*100, 2)}%, mrg0-mrg3 of mrg0)', summary_file_name, 2, True)
+    checkpoint_to_logfile(f'\t> {gwr_mrg3["EGID"].nunique()} gwr_mrg3 EGIDS {round((gwr_mrg3["EGID"].nunique())/gwr_mrg0["EGID"].nunique()*100, 2)}%  of  {gwr_mrg0["EGID"].nunique()} gwr_mrg0', summary_file_name, 2, True)
+    print_to_logfile(f'\n', log_file_name_def=summary_file_name)
 
 
     # check proxy possiblity -------------------
     checkpoint_to_logfile(f'gwr_guilding_df.shape: {gwr_building_df.shape}, EGID: {gwr_building_df["EGID"].nunique()};\n  gwr_dwelling_df.shape: {gwr_dwelling_df.shape}, EGID: {gwr_dwelling_df["EGID"].nunique()};\n  gwr.shape: {gwr.shape}, EGID: {gwr["EGID"].nunique()}', log_file_name_def, 2, True)
     
-    checkpoint_to_logfile(f'* check for WAZIM: {gwr.loc[gwr["WAZIM"] != "", "EGID"].nunique()} unique EGIDs of non-empty WAZIM", {round(gwr.loc[gwr["WAZIM"] != "", "EGID"].nunique() / gwr["EGID"].nunique() * 100, 2)} % of total unique EGIDs ({gwr["EGID"].nunique()})', log_file_name_def, 1, True)
-    checkpoint_to_logfile(f'* check for WAREA: {gwr.loc[gwr["WAREA"] != "", "EGID"].nunique()} unique EGIDs of non-empty WAREA", {round(gwr.loc[gwr["WAREA"] != "", "EGID"].nunique() / gwr["EGID"].nunique() * 100, 2)} % of total unique EGIDs ({gwr["EGID"].nunique()})', log_file_name_def, 1, True)
-    checkpoint_to_logfile(f'* check for GAREA: {gwr.loc[gwr["GAREA"] != "", "EGID"].nunique()} unique EGIDs of non-empty GAREA", {round(gwr.loc[gwr["GAREA"] != "", "EGID"].nunique() / gwr["EGID"].nunique() * 100, 2)} % of total unique EGIDs ({gwr["EGID"].nunique()})', log_file_name_def, 1, True)
-    
+    checkpoint_to_logfile(f'* check for WAZIM: {gwr.loc[gwr["WAZIM"] != "", "EGID"].nunique()} unique EGIDs of non-empty WAZIM", {round((gwr.loc[gwr["WAZIM"] != "", "EGID"].nunique() ) / gwr["EGID"].nunique() * 100, 2)} % of total unique EGIDs ({gwr["EGID"].nunique()})', log_file_name_def, 1, True)
+    checkpoint_to_logfile(f'* check for WAREA: {gwr.loc[gwr["WAREA"] != "", "EGID"].nunique()} unique EGIDs of non-empty WAREA", {round((gwr.loc[gwr["WAREA"] != "", "EGID"].nunique() ) / gwr["EGID"].nunique() * 100, 2)} % of total unique EGIDs ({gwr["EGID"].nunique()})', log_file_name_def, 1, True)
+    checkpoint_to_logfile(f'* check for GAREA: {gwr.loc[gwr["GAREA"] != "", "EGID"].nunique()} unique EGIDs of non-empty GAREA", {round((gwr.loc[gwr["GAREA"] != "", "EGID"].nunique() ) / gwr["EGID"].nunique() * 100, 2)} % of total unique EGIDs ({gwr["EGID"].nunique()})', log_file_name_def, 1, True)
+
     # checkpoint_to_logfile('Did NOT us a combination of building and dwelling data, \n because they overlap way too little. This makes sense \nintuitievly as single unit houses probably are not registered \nas dwellings in the data base.', log_file_name_def, 1, True)
 
 
     # merge dfs and export -------------------
     # gwr = gwr_building_df
     gwr.to_csv(f'{data_path_def}/output/preprep_data/gwr.csv', sep=';', index=False)
+    gwr_mrg_all_building_in_bfs.to_csv(f'{data_path_def}/output/preprep_data/gwr_mrg_all_building_in_bfs.csv', sep=';', index=False)
     gwr.to_parquet(f'{data_path_def}/output/preprep_data/gwr.parquet')
+    gwr_mrg_all_building_in_bfs.to_parquet(f'{data_path_def}/output/preprep_data/gwr_mrg_all_building_in_bfs.parquet')
     checkpoint_to_logfile(f'exported gwr data', log_file_name_def=log_file_name_def, n_tabs_def = 4)
 
 
