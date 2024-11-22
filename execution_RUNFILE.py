a59c6f74da2fb22b13db5c1ccf647663e48d187c
raulhochuliunibas <raul.hochuli@unibas.ch>
2024-11-22 16:50:24 +0100

    push for server run

--- execution_RUNFILE.py
+++ execution_RUNFILE.py
@@ -1,4 +1,3 @@
-import os
 import data_aggregation_MASTER, pvalloc_initialization_MASTER, pvalloc_MCalgorithm_MASTER, visualization_MASTER
 #mport pvalloc_postprocessing_MASTER
 
@@ -8,10 +7,10 @@ from visualisations.defaults_settings import extend_visual_sett_with_defaults
 
 
 # SETTINGS DEFINITION ==================================================================================================================
-months_pred = 12 #600 #36
+months_pred = 48 #600 #36
 MC_iter = 2
 run_on_server = False
-bfs_numbers = [2768, 2761, 2473, 2475, 2785, 2480] # Laufen & Umgebung > [2791, 2787, 2792, 2784, 2793, 2782, 2781,]
+bfs_numbers = [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475] # Laufen & Umgebung > [2791, 2787, 2792, 2784, 2793, 2782, 2781,] # Breitenbach & Umgebung [2617, 2615, 2614, 2613, 2782, 2620, 2622]
 
 run_dataagg =       False
 run_alloc_init =    True
@@ -45,78 +44,48 @@ dataagg_scenarios = extend_dataag_scen_with_defaults(dataagg_scenarios)
 
 # pv_allocation 
 pvalloc_scenarios={
-    f'pvalloc_DEV_{months_pred}m_meth2_npv':{
-        'name_dir_import': 'preprep_BL_22to23_1and2homes',
-        'script_run_on_server': run_on_server,
-        'months_prediction': months_pred,
-        # 'kt_numbers': [13,],
-        'bfs_numbers': bfs_numbers,
-        'recreate_topology':             True, 
-        'recalc_economics_topo_df':      True,
-        'sanitycheck_byEGID':            True,
-        'create_gdf_export_of_topology': True,
-
-        # 'sanitycheck_summary_byEGID_specs':{
-        #     'n_iterations_before_sanitycheck': 2,},
-        'algorithm_specs': {
-            'inst_selection_method': 'prob_weighted_npv',},
-        'tech_economic_specs': {
-            'max_distance_m_for_EGID_node_matching': 0,
-            'self_consumption_ifapplicable': 0,
-            'pvprod_calc_method': 'method2',},
-        'MC_loop_specs': {
-            'montecarlo_iterations': MC_iter,},
-},
-
-    # f'pvalloc_DEV_{months_pred}m_meth2_rand':{
+    # f'pvalloc_DEV_{months_pred}m_meth2_npv':{
     #     'name_dir_import': 'preprep_BL_22to23_1and2homes',
     #     'script_run_on_server': run_on_server,
     #     'months_prediction': months_pred,
     #     # 'kt_numbers': [13,],
     #     'bfs_numbers': bfs_numbers,
-    #     'recreate_topology':             False, 
-    #     'recalc_economics_topo_df':      False,
+    #     'recreate_topology':             True, 
+    #     'recalc_economics_topo_df':      True,
     #     'sanitycheck_byEGID':            True,
     #     'create_gdf_export_of_topology': True,
+
+    #     # 'sanitycheck_summary_byEGID_specs':{
+    #     #     'n_iterations_before_sanitycheck': 2,},
     #     'algorithm_specs': {
-    #         'inst_selection_method': 'random',},
+    #         'inst_selection_method': 'prob_weighted_npv',},
     #     'tech_economic_specs': {
     #         'max_distance_m_for_EGID_node_matching': 0,
     #         'self_consumption_ifapplicable': 0,
-    #         },
+    #         'pvprod_calc_method': 'method2',},
     #     'MC_loop_specs': {
     #         'montecarlo_iterations': MC_iter,},
     # },
-  
-}
-
-parklplatz = {
-    f'pvalloc_DEV_{months_pred}m_meth2_selfcon00_DirDiffRad':{
-        'name_dir_import': 'preprep_BL_20to22_1and2homes_buff002',
+    f'pvalloc_{months_pred}m_meth2_npv':{
+        'name_dir_import': 'preprep_BL_22to23_1and2homes',
         'script_run_on_server': run_on_server,
         'months_prediction': months_pred,
         'bfs_numbers': bfs_numbers,
         'tech_economic_specs': {
-            'self_consumption_ifapplicable': 0,
             'pvprod_calc_method': 'method2',},
-        'algorithm_specs': {
-            'rand_seed': 42,},
-        'weather_specs': {
-            'meteo_col_radiation_proxy': ['Basel Direct Shortwave Radiation','Basel Diffuse Shortwave Radiation',]}
-        },
-    f'pvalloc_DEV_{months_pred}m_meth3_selfcon00_DirRad':{
-        'name_dir_import': 'preprep_BL_20to22_1and2homes_buff002',
+        'MC_loop_specs': {
+            'montecarlo_iterations': MC_iter,},
+    },
+        f'pvalloc_{months_pred}m_meth3_npv':{
+        'name_dir_import': 'preprep_BL_22to23_1and2homes',
         'script_run_on_server': run_on_server,
         'months_prediction': months_pred,
         'bfs_numbers': bfs_numbers,
         'tech_economic_specs': {
-            'self_consumption_ifapplicable': 0,
             'pvprod_calc_method': 'method3',},
-        'algorithm_specs': {
-            'rand_seed': 42,},
-        'weather_specs': {
-            'meteo_col_radiation_proxy': ['Basel Direct Shortwave Radiation',]}
-        },  
+        'MC_loop_specs': {
+            'montecarlo_iterations': MC_iter,},
+    },
 }
 pvalloc_scenarios = extend_pvalloc_scen_with_defaults(pvalloc_scenarios)
 
@@ -127,21 +96,25 @@ visual_settings = {
         'MC_subdir_for_plot': 'ASDFASDF', 
         'node_selection_for_plots': ['node1', 'node10', 'node15'], # or None for all nodes
 
-        # for pvalloc_initalization + sanity check
-        'plot_ind_var_summary_stats':            True,
-        'plot_ind_charac_omitted_gwr':           False,
+        # PLOT IND for pvalloc_initalization + sanity check
+        'plot_ind_var_summary_stats':            True,        # |> bookmark: add a histplot for pvprod_kwh.sum and STROMERTRAG.sum for comparison
+        'plot_ind_charac_omitted_gwr':           False,       # |> bookmark: if possible find a way to continue script, even if plots are shown
+        #NEU plot radiation over time
+        #NEU 'plot_ind_hist_installedCap_kw': > in plot_ind_var_summary_stats? 
+        #NEU plot_ind_hist_annualpvprod_kwh : > in plot_ind_var_summary_stats?
 
 
-        # for pvalloc_MC_algorithm 
+        # PLOT IND for pvalloc_MC_algorithm 
 
-        'plot_ind_line_productionHOY_per_node':  False,
-        'plot_ind_line_installedCap_per_month':  False,
+        'plot_ind_line_installedCap':            False,       # |> bookmark: Necessary to specify which MC iteration to plot! ctrlF: "MC_subdir_for_plot"
+        'plot_ind_line_productionHOY_per_node':  False,      
+        # 'plot_ind_line_installedCap_per_month':  False,     # |> bookmark: remove 
         'plot_ind_hist_NPV_freepartitions':      False,
-        #NEU 'plot_ind_hist_installedCap_kw':
-        #NEU plot_ind_hist_annualpvprod_kwh:
+
 
         # for aggregated pvalloc_MC_algorithm 
 
+        # PLOT IND for maps
 
         #                                      # False,
         'plot_ind_map_topo_egid':                False,
