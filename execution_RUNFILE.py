d268ea2d213e8b27b8c9c0716d7d0750acba0394
raulhochuliunibas <raul.hochuli@unibas.ch>
2024-11-28 20:18:05 +0100

    push for server run

--- execution_RUNFILE.py
+++ execution_RUNFILE.py
@@ -115,12 +115,69 @@ pvalloc_scenarios={
     #     'MC_loop_specs': {
     #         'montecarlo_iterations': 1,},
     # },   
-    f'pvalloc_sample_12m_meth2_rnd_diffratio05':{
+    # f'pvalloc_sample_12m_meth2_rnd_diffratio05':{
+    #     'name_dir_import': 'preprep_BL_22to23_1and2homes',
+    #     'script_run_on_server': run_on_server,
+    #     'months_prediction': 12,
+    #     'weather_specs': {
+    #         'diffuse_to_direct_rad_factor': 0.5,},
+    #     'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
+    #     'algorithm_specs': {
+    #         'inst_selection_method': 'random',},
+    #     'tech_economic_specs': {
+    #         'pvprod_calc_method': 'method2',},
+    #     'MC_loop_specs': {
+    #         'montecarlo_iterations': 1,},
+    # },
+    # f'pvalloc_sample_12m_meth3_rnd_diffratio05':{
+    #     'name_dir_import': 'preprep_BL_22to23_1and2homes',
+    #     'script_run_on_server': run_on_server,
+    #     'months_prediction': 12,
+    #     'weather_specs': {
+    #         'diffuse_to_direct_rad_factor': 0.5,},
+    #     'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
+    #     'algorithm_specs': {
+    #         'inst_selection_method': 'random',},
+    #     'tech_economic_specs': {
+    #         'pvprod_calc_method': 'method3',},
+    #     'MC_loop_specs': {
+    #         'montecarlo_iterations': 1,},
+    # },   
+    # f'pvalloc_sample_12m_meth2_rnd_diffratio08':{
+    #     'name_dir_import': 'preprep_BL_22to23_1and2homes',
+    #     'script_run_on_server': run_on_server,
+    #     'months_prediction': 12,
+    #     'weather_specs': {
+    #         'diffuse_to_direct_rad_factor': 0.8,},
+    #     'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
+    #     'algorithm_specs': {
+    #         'inst_selection_method': 'random',},
+    #     'tech_economic_specs': {
+    #         'pvprod_calc_method': 'method2',},
+    #     'MC_loop_specs': {
+    #         'montecarlo_iterations': 1,},
+    # },
+    # f'pvalloc_sample_12m_meth3_rnd_diffratio08':{
+    #     'name_dir_import': 'preprep_BL_22to23_1and2homes',
+    #     'script_run_on_server': run_on_server,
+    #     'months_prediction': 12,
+    #     'weather_specs': {
+    #         'diffuse_to_direct_rad_factor': 0.8,},
+    #     'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
+    #     'algorithm_specs': {
+    #         'inst_selection_method': 'random',},
+    #     'tech_economic_specs': {
+    #         'pvprod_calc_method': 'method3',},
+    #     'MC_loop_specs': {
+    #         'montecarlo_iterations': 1,},
+    # },
+
+    f'pvalloc_sample_12m_meth2_rnd_diffratio10':{
         'name_dir_import': 'preprep_BL_22to23_1and2homes',
         'script_run_on_server': run_on_server,
         'months_prediction': 12,
         'weather_specs': {
-            'diffuse_to_direct_rad_factor': 0.5,},
+            'diffuse_to_direct_rad_factor': 1,},
         'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
         'algorithm_specs': {
             'inst_selection_method': 'random',},
@@ -129,12 +186,12 @@ pvalloc_scenarios={
         'MC_loop_specs': {
             'montecarlo_iterations': 1,},
     },
-    f'pvalloc_sample_12m_meth3_rnd_diffratio05':{
+    f'pvalloc_sample_12m_meth3_rnd_diffratio10':{
         'name_dir_import': 'preprep_BL_22to23_1and2homes',
         'script_run_on_server': run_on_server,
         'months_prediction': 12,
         'weather_specs': {
-            'diffuse_to_direct_rad_factor': 0.5,},
+            'diffuse_to_direct_rad_factor': 1,},
         'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
         'algorithm_specs': {
             'inst_selection_method': 'random',},
@@ -142,13 +199,14 @@ pvalloc_scenarios={
             'pvprod_calc_method': 'method3',},
         'MC_loop_specs': {
             'montecarlo_iterations': 1,},
-    },   
-    f'pvalloc_sample_12m_meth2_rnd_diffratio08':{
+    }, 
+
+    f'pvalloc_sample_12m_meth2_rnd_diffratio12':{
         'name_dir_import': 'preprep_BL_22to23_1and2homes',
         'script_run_on_server': run_on_server,
         'months_prediction': 12,
         'weather_specs': {
-            'diffuse_to_direct_rad_factor': 0.8,},
+            'diffuse_to_direct_rad_factor': 1.2,},
         'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
         'algorithm_specs': {
             'inst_selection_method': 'random',},
@@ -157,12 +215,12 @@ pvalloc_scenarios={
         'MC_loop_specs': {
             'montecarlo_iterations': 1,},
     },
-    f'pvalloc_sample_12m_meth3_rnd_diffratio08':{
+    f'pvalloc_sample_12m_meth3_rnd_diffratio12':{
         'name_dir_import': 'preprep_BL_22to23_1and2homes',
         'script_run_on_server': run_on_server,
         'months_prediction': 12,
         'weather_specs': {
-            'diffuse_to_direct_rad_factor': 0.8,},
+            'diffuse_to_direct_rad_factor': 1.2,},
         'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
         'algorithm_specs': {
             'inst_selection_method': 'random',},
@@ -171,7 +229,122 @@ pvalloc_scenarios={
         'MC_loop_specs': {
             'montecarlo_iterations': 1,},
     },
-    
+
+    f'pvalloc_sample_12m_meth2_rnd_diffratio14':{
+        'name_dir_import': 'preprep_BL_22to23_1and2homes',
+        'script_run_on_server': run_on_server,
+        'months_prediction': 12,
+        'weather_specs': {
+            'diffuse_to_direct_rad_factor': 1.4,},
+        'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
+        'algorithm_specs': {
+            'inst_selection_method': 'random',},
+        'tech_economic_specs': {
+            'pvprod_calc_method': 'method2',},
+        'MC_loop_specs': {
+            'montecarlo_iterations': 1,},
+    },
+    f'pvalloc_sample_12m_meth3_rnd_diffratio14':{
+        'name_dir_import': 'preprep_BL_22to23_1and2homes',
+        'script_run_on_server': run_on_server,
+        'months_prediction': 12,
+        'weather_specs': {
+            'diffuse_to_direct_rad_factor': 1.4,},
+        'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
+        'algorithm_specs': {
+            'inst_selection_method': 'random',},
+        'tech_economic_specs': {
+            'pvprod_calc_method': 'method3',},
+        'MC_loop_specs': {
+            'montecarlo_iterations': 1,},
+    },
+
+    f'pvalloc_sample_12m_meth2_rnd_diffratio16':{
+        'name_dir_import': 'preprep_BL_22to23_1and2homes',
+        'script_run_on_server': run_on_server,
+        'months_prediction': 12,
+        'weather_specs': {
+            'diffuse_to_direct_rad_factor': 1.6,},
+        'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
+        'algorithm_specs': {
+            'inst_selection_method': 'random',},
+        'tech_economic_specs': {
+            'pvprod_calc_method': 'method2',},
+        'MC_loop_specs': {
+            'montecarlo_iterations': 1,},
+    },
+    f'pvalloc_sample_12m_meth3_rnd_diffratio16':{
+        'name_dir_import': 'preprep_BL_22to23_1and2homes',
+        'script_run_on_server': run_on_server,
+        'months_prediction': 12,
+        'weather_specs': {
+            'diffuse_to_direct_rad_factor': 1.6,},
+        'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
+        'algorithm_specs': {
+            'inst_selection_method': 'random',},
+        'tech_economic_specs': {
+            'pvprod_calc_method': 'method3',},
+        'MC_loop_specs': {
+            'montecarlo_iterations': 1,},
+    },
+
+    f'pvalloc_sample_12m_meth2_rnd_diffratio18':{
+        'name_dir_import': 'preprep_BL_22to23_1and2homes',
+        'script_run_on_server': run_on_server,
+        'months_prediction': 12,
+        'weather_specs': {
+            'diffuse_to_direct_rad_factor': 1.8,},
+        'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
+        'algorithm_specs': {
+            'inst_selection_method': 'random',},
+        'tech_economic_specs': {
+            'pvprod_calc_method': 'method2',},
+        'MC_loop_specs': {
+            'montecarlo_iterations': 1,},
+    },
+    f'pvalloc_sample_12m_meth3_rnd_diffratio18':{
+        'name_dir_import': 'preprep_BL_22to23_1and2homes',
+        'script_run_on_server': run_on_server,
+        'months_prediction': 12,
+        'weather_specs': {
+            'diffuse_to_direct_rad_factor': 1.8,},
+        'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
+        'algorithm_specs': {
+            'inst_selection_method': 'random',},
+        'tech_economic_specs': {
+            'pvprod_calc_method': 'method3',},
+        'MC_loop_specs': {
+            'montecarlo_iterations': 1,},
+    },
+
+    f'pvalloc_sample_12m_meth2_rnd_diffratio20':{
+        'name_dir_import': 'preprep_BL_22to23_1and2homes',
+        'script_run_on_server': run_on_server,
+        'months_prediction': 12,
+        'weather_specs': {
+            'diffuse_to_direct_rad_factor': 2,},
+        'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
+        'algorithm_specs': {
+            'inst_selection_method': 'random',},
+        'tech_economic_specs': {
+            'pvprod_calc_method': 'method2',},
+        'MC_loop_specs': {
+            'montecarlo_iterations': 1,},
+    },
+    f'pvalloc_sample_12m_meth3_rnd_diffratio20':{
+        'name_dir_import': 'preprep_BL_22to23_1and2homes',
+        'script_run_on_server': run_on_server,
+        'months_prediction': 12,
+        'weather_specs': {
+            'diffuse_to_direct_rad_factor': 2,},
+        'bfs_numbers': [2768, 2761, 2772, 2473, 2475, 2785, 2480, 2475],
+        'algorithm_specs': {
+            'inst_selection_method': 'random',},
+        'tech_economic_specs': {
+            'pvprod_calc_method': 'method3',},
+        'MC_loop_specs': {
+            'montecarlo_iterations': 1,},
+    },
 }
 pvalloc_scenarios = extend_pvalloc_scen_with_defaults(pvalloc_scenarios)
 
