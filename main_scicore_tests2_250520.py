8907111840627b46f82c7a5e8cbec8afd945cb7e
raulhochuliunibas <raul.hochuli@unibas.ch>
2025-06-01 12:25:49 +0200

    adjust test2 scenarios for longer time range and yearly observations

--- main_scicore_tests2_250520.py
+++ main_scicore_tests2_250520.py
@@ -5,11 +5,29 @@ from src.MAIN_visualization import Visual_Settings, Visualization
 pvalloc_scen_list = [
     
     PVAllocScenario_Settings(
-        name_dir_export                 = 'pvalloc_BLsml_test2_default_w_pvdf_adjust',
+        name_dir_export                 = 'pvalloc_BLsml_test2_default_w_pvdf_adjust_rnd',
         name_dir_import                 = 'preprep_BL_22to23_extSolkatEGID',
         bfs_numbers                     = [2767, 2771, 2765, 2764,  ], 
         T0_year_prediction              = 2021,
-        months_prediction               = 120,
+        months_prediction               = 360,
+        CSTRspec_iter_time_unit         = 'year',
+        CHECKspec_n_iterations_before_sanitycheck   = 2,
+        ALGOspec_inst_selection_method              = 'random', 
+        ALGOspec_rand_seed                          = 123,
+        TECspec_pvprod_calc_method                  = 'method2.2',
+        MCspec_montecarlo_iterations                = 2,
+        ALGOspec_adjust_existing_pvdf_pvprod_bypartition_TF = True,
+
+        ALGOspec_subselec_filter_criteria = None, 
+        ),
+
+    PVAllocScenario_Settings(
+        name_dir_export                 = 'pvalloc_BLsml_test2_default_w_pvdf_adjust_max',
+        name_dir_import                 = 'preprep_BL_22to23_extSolkatEGID',
+        bfs_numbers                     = [2767, 2771, 2765, 2764,  ], 
+        T0_year_prediction              = 2021,
+        months_prediction               = 360,
+        CSTRspec_iter_time_unit         = 'year',
         CHECKspec_n_iterations_before_sanitycheck   = 2,
         ALGOspec_inst_selection_method              = 'max_npv', 
         ALGOspec_rand_seed                          = 123,
@@ -25,7 +43,8 @@ pvalloc_scen_list = [
         name_dir_import                 = 'preprep_BL_22to23_extSolkatEGID',
         bfs_numbers                     = [2767, 2771, 2765, 2764,  ], 
         T0_year_prediction              = 2021,
-        months_prediction               = 120,
+        months_prediction               = 360,
+        CSTRspec_iter_time_unit         = 'year',
         CHECKspec_n_iterations_before_sanitycheck   = 2,
         ALGOspec_inst_selection_method              = 'max_npv', 
         ALGOspec_rand_seed                          = 123,
@@ -41,7 +60,8 @@ pvalloc_scen_list = [
         name_dir_import                 = 'preprep_BL_22to23_extSolkatEGID',
         bfs_numbers                     = [2767, 2771, 2765, 2764,  ], 
         T0_year_prediction              = 2021,
-        months_prediction               = 120,
+        months_prediction               = 360,
+        CSTRspec_iter_time_unit         = 'year',
         CHECKspec_n_iterations_before_sanitycheck   = 2,
         ALGOspec_inst_selection_method              = 'max_npv', 
         ALGOspec_rand_seed                          = 123,
@@ -57,7 +77,8 @@ pvalloc_scen_list = [
         name_dir_import                 = 'preprep_BL_22to23_extSolkatEGID',
         bfs_numbers                     = [2767, 2771, 2765, 2764,  ], 
         T0_year_prediction              = 2021,
-        months_prediction               = 120,
+        months_prediction               = 360,
+        CSTRspec_iter_time_unit         = 'year',
         CHECKspec_n_iterations_before_sanitycheck   = 2,
         ALGOspec_inst_selection_method              = 'max_npv', 
         ALGOspec_rand_seed                          = 123,
@@ -75,9 +96,10 @@ visualization_list = [
         Visual_Settings(
             pvalloc_exclude_pattern_list = [
                 '*.txt','*old_vers*', 
-                '*test1*',
-                '*test3*',
                 ], 
+            pvalloc_include_pattern_list = [
+                '*test2*', 
+            ],
             save_plot_by_scen_directory        = True, 
             remove_old_plot_scen_directories   = False,  
             remove_old_plots_in_visualization = False,  
@@ -100,18 +122,18 @@ if __name__ == '__main__':
         visual_class = Visualization(visual_scen)
 
         # # -- def plot_ALL_init_sanitycheck(self, ): -------------
-        visual_class.plot_ind_var_summary_stats()                     # runs as intended
-        visual_class.plot_ind_hist_pvcapaprod_sanitycheck()           # runs as intended
+        # visual_class.plot_ind_var_summary_stats()                     # runs as intended
+        # visual_class.plot_ind_hist_pvcapaprod_sanitycheck()           # runs as intended
         # # visual_class.plot_ind_boxp_radiation_rng_sanitycheck()
-        visual_class.plot_ind_charac_omitted_gwr()                    # runs as intended
-        visual_class.plot_ind_line_meteo_radiation()                  # runs as intended
+        # visual_class.plot_ind_charac_omitted_gwr()                    # runs as intended
+        # visual_class.plot_ind_line_meteo_radiation()                  # runs as intended
 
         # # -- def plot_ALL_mcalgorithm(self,): -------------
         visual_class.plot_ind_line_installedCap()                     # runs as intended
         visual_class.plot_ind_line_productionHOY_per_node()           # runs as intended
         visual_class.plot_ind_line_productionHOY_per_EGID()           # runs as intended
         # visual_class.plot_ind_line_PVproduction()                     # runs
-        visual_class.plot_ind_hist_NPV_freepartitions()               # runs as intended
+        # visual_class.plot_ind_hist_NPV_freepartitions()               # runs as intended
         visual_class.plot_ind_line_gridPremiumHOY_per_node()          # runs 
         visual_class.plot_ind_line_gridPremium_structure()            # runs 
 
