07c12f2f52d4fef5bb93d790c2d90457c61f3f97
raulhochuliunibas <raul.hochuli@unibas.ch>
2025-07-07 09:39:54 +0200

    start with faster c instead of slower a in test2 scenarios...

--- main_scicore_visualization.py
+++ main_scicore_visualization.py
@@ -13,28 +13,28 @@ visualization_list = [
                 '*test2_*'
                 ], 
             pvalloc_include_pattern_list = [
-                '*test2a*_selfcons1',
+                '*test2c*_selfcons1',
                 # 'pvalloc_BLsml_test2_default_w_pvdf_adjust_max_FORPLOT',  
             ],
-            # save_plot_by_scen_directory        = True, 
-            # remove_old_plot_scen_directories   = True,  
-            # remove_old_plots_in_visualization  = True,  
-            # remove_old_csvs_in_visualization   = True,
+            save_plot_by_scen_directory        = True, 
+            remove_old_plot_scen_directories   = True,  
+            remove_old_plots_in_visualization  = True,  
+            remove_old_csvs_in_visualization   = True,
             ),   
-        Visual_Settings(
-            pvalloc_include_pattern_list = ['*test2b*_selfcons1',],),  
-        Visual_Settings(
-            pvalloc_include_pattern_list = ['*test2c*_selfcons1',],),   
+        # Visual_Settings(
+        #     pvalloc_include_pattern_list = ['*test2b*_selfcons1',],),  
+        # Visual_Settings(
+            # pvalloc_include_pattern_list = ['*test2a*_selfcons1',],),   
             
             
-        Visual_Settings(
-            pvalloc_include_pattern_list = ['pvalloc*_default_rnd_selfcons1',],),         
-        Visual_Settings(
-            pvalloc_include_pattern_list = ['pvalloc*_southfacing_rnd_selfcons1',],),
-        Visual_Settings(
-            pvalloc_include_pattern_list = ['pvalloc*_eastwestfacing_rnd_selfcons1',],),
-        Visual_Settings(
-            pvalloc_include_pattern_list = ['pvalloc*_default_max_selfcons1',],),
+        # Visual_Settings(
+        #     pvalloc_include_pattern_list = ['pvalloc*_default_rnd_selfcons1',],),         
+        # Visual_Settings(
+        #     pvalloc_include_pattern_list = ['pvalloc*_southfacing_rnd_selfcons1',],),
+        # Visual_Settings(
+        #     pvalloc_include_pattern_list = ['pvalloc*_eastwestfacing_rnd_selfcons1',],),
+        # Visual_Settings(
+        #     pvalloc_include_pattern_list = ['pvalloc*_default_max_selfcons1',],),
 
     ]    
 
@@ -46,26 +46,28 @@ if __name__ == '__main__':
         visual_class = Visualization(visual_scen)
 
         plot_method_names = [
+            "plot_ind_hist_cols_HOYagg_per_EGID",                    # runs as intended
+
 
             # # # -- def plot_ALL_init_sanitycheck(self, ): -------------
-            # "plot_ind_var_summary_stats",                      # runs as intended
-            # "plot_ind_hist_pvcapaprod_sanitycheck",            # runs as intended
-            # # # "plot_ind_boxp_radiation_rng_sanitycheck", 
-            # "plot_ind_charac_omitted_gwr",                     # runs as intended
+            "plot_ind_var_summary_stats",                      # runs as intended
+            "plot_ind_hist_pvcapaprod_sanitycheck",            # runs as intended
+            # # "plot_ind_boxp_radiation_rng_sanitycheck", 
+            "plot_ind_charac_omitted_gwr",                     # runs as intended
             # # "plot_ind_line_meteo_radiation",                   # runs as intended
 
             # # -- def plot_ALL_mcalgorithm(self,): -------------
             # # "plot_ind_line_installedCap",                     # runs as intended
-            # "plot_ind_line_productionHOY_per_node",           # runs as intended
-            # "plot_ind_line_productionHOY_per_EGID",           # runs as intended
-            "plot_ind_hist_cols_HOYagg_per_EGID",                    # runs as intended
-            # "plot_ind_line_PVproduction",                   # runs — optional, uncomment if needed
-            # "plot_ind_hist_NPV_freepartitions",               # runs as intended
+            "plot_ind_line_productionHOY_per_node",           # runs as intended
+            "plot_ind_line_productionHOY_per_EGID",           # runs as intended
+            # "plot_ind_hist_cols_HOYagg_per_EGID",                    # runs as intended
+            "plot_ind_line_PVproduction",                   # runs — optional, uncomment if needed
+            # # "plot_ind_hist_NPV_freepartitions",               # runs as intended
             # # "plot_ind_line_gridPremiumHOY_per_node",          # runs
             # # "plot_ind_line_gridPremium_structure",            # runs
             # # "plot_ind_lineband_contcharact_newinst",          # status not noted
-            # "plot_ind_map_topo_egid",                         # runs as intended
-            # "plot_ind_map_topo_egid_incl_gridarea",         # runs as intended — optional
+            "plot_ind_map_topo_egid",                         # runs as intended
+            "plot_ind_map_topo_egid_incl_gridarea",         # runs as intended — optional
             # # "plot_ind_map_node_connections"                   # status not noted        
         
             # visual_class.plot_ind_boxp_radiation_rng_sanitycheck()
